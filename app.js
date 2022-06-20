11beac2b856da44bb9bae60cd3205ae49c64f51f
s1f102100119 <s1f102100119@iniad.org>
2022-06-20 13:21:11 +0900

    express-generatorを使用

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express  = require("express");
-
-const app = express();
+const createError = require('http-errors');
+const express = require('express');
 const path = require('path');
-const api = require('./api/reversi.js');
-app.use('/api', api);
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-app.listen(8000, () => {
-    console.log('Running at Port 8000...');
-  });
+const apiRouter = require('./routes/api');
+const app = express();
 
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, DELETE, OPTION"
-    )
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    next()
-  })
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/api', apiRouter);
 
-  app.get("/", (req, res) =>{
-    res.send('Hello World!');
-    console.log("/ へアクセスがありました");
-  });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTION"
+  )
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  next()
+})
+
+app.get("/", (req, res) =>{
+  res.send('Hello World!');
+  console.log("/ へアクセスがありました");
+});
+
+module.exports = app;
