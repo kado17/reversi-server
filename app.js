9e4a408549e733ca78388d9ce0bede770833b5d4
s1f102100119 <s1f102100119@iniad.org>
2022-06-20 15:40:08 +0900

    eslintとprettierを追加

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+//const createError = require("http-errors");
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-const apiRouter = require('./routes/api');
-const app = express();
+const apiRouter = require('./routes/api')
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use('/api', apiRouter);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use('/api', apiRouter)
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTION"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTION'
   )
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
 })
 
-app.get("/", (req, res) =>{
-  res.send('Hello World!');
-  console.log("/ へアクセスがありました");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log('/ へアクセスがありました')
+})
 
-module.exports = app;
+module.exports = app
--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const reversi = require('../public/javascripts/reversi');
+const express = require('express')
+const router = express.Router()
+const reversi = require('../public/javascripts/reversi')
 
 let board = reversi.board
 
 router.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTION"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTION'
   )
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
 })
 
 router.get('/test', function (req, res) {
   board = reversi.board_init()
   res.json({
-      board: board
-    });
-});
+    board: board,
+  })
+})
 
-module.exports = router;
+module.exports = router
