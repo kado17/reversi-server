bbc8f1e4c791c335ffae7405be33a398ae6ddd5e
s1f102100119 <s1f102100119@iniad.org>
2022-06-25 01:53:13 +0900

    オセロ石を置く仮処理の追加

--- public/javascripts/reversi.js
+++ public/javascripts/reversi.js
@@ -1,29 +1,132 @@
 const SIZE = 8
 const WIDTH = SIZE
 const HEIGHT = SIZE
+const WHITE = 0,
+  BLACK = 1,
+  //ISPUTABLE = 8,
+  EMPTY = 9
 
-let board = [
-  [9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9],
-  [1, 0, 1, 0, 0, 0, 1, 0],
-  [0, 1, 0, 0, 0, 1, 0, 1],
-  [9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9],
-]
-
-exports.board_init = () => {
+exports.boardInit = () => {
   let new_board = JSON.parse(
-    JSON.stringify(new Array(8).fill(new Array(8).fill(9)))
+    JSON.stringify(new Array(8).fill(new Array(8).fill(EMPTY)))
   )
-  new_board[HEIGHT / 2][WIDTH / 2] = 0
-  new_board[HEIGHT / 2 - 1][WIDTH / 2 - 1] = 0
-  new_board[HEIGHT / 2 - 1][WIDTH / 2] = 1
-  new_board[HEIGHT / 2][WIDTH / 2 - 1] = 1
+  new_board[HEIGHT / 2][WIDTH / 2] = WHITE
+  new_board[HEIGHT / 2 - 1][WIDTH / 2 - 1] = WHITE
+  new_board[HEIGHT / 2 - 1][WIDTH / 2] = BLACK
+  new_board[HEIGHT / 2][WIDTH / 2 - 1] = BLACK
   return new_board
 }
 
+let board = JSON.parse(
+  JSON.stringify(this.boardInit()))
+
+exports.isPutableDisk = (x, y, oneself, board) => {
+  if (board[y][x] != EMPTY) {
+    return false
+  }
+
+  let opponent
+  if (oneself == WHITE) {
+    opponent = BLACK
+  } else if (oneself == BLACK) {
+    opponent = WHITE
+  } else {
+    return false
+  }
+
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      if (i == 0 && j == 0) {
+        continue
+      }
+
+      if (y + j < 0 || HEIGHT < y + j || x + i < 0 || WIDTH < x + i) {
+        continue
+      }
+
+      if (board[y + j][x + i] != opponent) {
+        continue
+      }
+
+      let tmpX, tmpY
+
+      for (let k = 2; k < SIZE; k++) {
+        tmpX = x + i * k
+        tmpY = y + j * k
+
+        if (tmpY < 0 || HEIGHT < tmpY || tmpX < 0 || WIDTH < tmpX) {
+          continue
+        }
+
+        if (board[tmpY][tmpX] == EMPTY) {
+          break
+        }
+
+        if (board[tmpY][tmpX] == oneself) {
+            return true
+          }
+        }
+      }
+  }
+  return false
+}
+
+exports.PutDisk = (x, y, oneself, board) => {
+  let new_board = JSON.parse(
+    JSON.stringify(board))
+
+  if (board[y][x] != EMPTY) {
+    return []
+  }
+
+  let opponent
+  if (oneself == WHITE) {
+    opponent = BLACK
+  } else if (oneself == BLACK) {
+    opponent = WHITE
+  } else {
+    return []
+  }
+
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      if (i == 0 && j == 0) {
+        continue
+      }
+
+      if (y + j < 0 || HEIGHT < y + j || x + i < 0 || WIDTH < x + i) {
+        continue
+      }
+
+      if (board[y + j][x + i] != opponent) {
+        continue
+      }
+
+      let tmpX, tmpY
+
+      for (let k = 2; k < SIZE; k++) {
+        tmpX = x + i * k
+        tmpY = y + j * k
+
+        if (tmpY < 0 || HEIGHT < tmpY || tmpX < 0 || WIDTH < tmpX) {
+          continue
+        }
+
+        if (board[tmpY][tmpX] == EMPTY) {
+          break
+        }
+
+        if (board[tmpY][tmpX] == oneself) {
+            new_board[y][x] = oneself
+            for (let n = 1; n < k; n++) {
+              new_board[y + j * n][x + i * n] = oneself
+            }
+          }
+        }
+      }
+    }
+  return new_board
+}
 
 
 exports.board = board
--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const reversi = require('../public/javascripts/reversi')
 
 let board = reversi.board
 router.use(express.json())
-router.use(express.urlencoded({ extended: true }));
+router.use(express.urlencoded({ extended: true }))
 
 router.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -18,15 +18,18 @@ router.use((req, res, next) => {
 })
 
 router.get('/test', function (req, res) {
-  board = reversi.board_init()
+  board = reversi.boardInit()
   res.json({
     board: board,
   })
 })
 
-router.post('/disk', function (req, res){
-  console.log(req.body)
-  res.send('Done')
+router.post('/disk', function (req, res) {
+  let { x, y } = req.body
+  let a = reversi.PutDisk(x,y,0,board)
+  console.log(a)
+  if (a.length)board = a
+  res.json({ a: board, x: x, y: y })
 })
 
 module.exports = router
