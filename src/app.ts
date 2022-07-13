9fbf34558fe5f5cb55816b0af4a46dce8fc1f5dc
s1f102100119 <s1f102100119@iniad.org>
2022-07-13 16:52:16 +0900

    ゲームステートの管理方法を変更

--- src/app.ts
+++ src/app.ts
@@ -1,124 +1,134 @@
 import http from 'http'
 import socketio from 'socket.io'
-import reversi from './reversi'
+import * as rev from './reversi'
 
 const PORT = 8000
 //サーバーの立ち上げ
 const server = http.createServer()
-const io = new socketio.Server(server,{cors: {
-  origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST'],
-}},)
-
-let board = reversi.board
-let turnColor = reversi.turnColor
-let entryPL: { [key: string]: string } = {}
-const entryPLKey = ['white', 'black']
-let isPlaying = false
-let diskCount = reversi.diskCount
+const io = new socketio.Server(server, {
+  cors: {
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+  },
+})
 
 const geneW8EntryMsg = (num: number) => `参加者募集中 現在${num}/2`
-const geneTurnPlayerMsg = (num: number) => `${reversi.conv[num]}の手番です`
-const geneWinnerMsg = (num: number) =>
-  num !== 9 ? `${reversi.conv[num]}の勝ちです` : '引き分けです'
+const geneTurnPlayerMsg = (PLColor: rev.PLColor) => `${rev.colorConvJp[PLColor]}の手番です`
+const geneWinnerMsg = (PLColor: rev.PLColor) =>
+  PLColor !== rev.PLColor.NA ? `${rev.colorConvJp[PLColor]}の勝ちです` : '引き分けです'
 
-let msg = geneW8EntryMsg(Object.keys(entryPL).length)
+const gameStateInit: rev.GameState = {
+  board: rev.createBoard(),
+  msg: geneW8EntryMsg(0),
+  turnColor: rev.PLColor.Black,
+  numberOfDisc: rev.getNumberOfDisc(rev.createBoard()),
+  isPlaying: false,
+  isGameOver: false,
+}
+
+const gameState = { ...gameStateInit }
+const entryPLInit: rev.EntryPL = { White: '', Black: '' }
+const entryPL = { ...entryPLInit }
+const geneEmitData = (keyList: rev.GSKey[]) => {
+  const result: { [key: string]: rev.GameStateType } = {}
+  Object.keys(gameState).forEach((key) => {
+    if (key in keyList) result[key] = gameState[key]
+  })
+  return result
+}
+
+const resetGameState = (): void => {
+  Object.keys(gameState).forEach((key) => {
+    gameState[key] = gameStateInit[key]
+  })
+}
+
+const gameOverProcessing = (isGameCancel: boolean) => {
+  const winner = rev.gameOver(gameState.numberOfDisc)
+  io.emit('gameOver', { isGameCancel: isGameCancel })
+  gameState.msg = geneWinnerMsg(winner)
+  gameState.isPlaying = false
+  gameState.isGameOver = false
+  io.emit('gameInfo', geneEmitData([rev.GSKey.msg, rev.GSKey.isPlaying, rev.GSKey.isGameOver]))
+}
 
 //socket処理を記載する
-io.on('connection', function (socket: socketio.Socket) {
+io.on('connection', (socket: socketio.Socket) => {
   console.log(socket.id, '接続!')
-  io.to(socket.id).emit('gameInfo', {
-    board: isPlaying ? reversi.setPutableCoord(turnColor, board) : board,
-    msg: msg,
-    diskCount: diskCount,
-    isPlaying: isPlaying,
-  })
+  io.to(socket.id).emit('gameInfo', gameState)
   //socket処理
 
-  socket.on('putDisk', (data) => {
+  socket.on('putDisc', (data) => {
     const { x, y } = data
-    const newBoard = reversi.PutDisk(x, y, turnColor, board)
-    const nextColor = reversi.colorChange(turnColor, newBoard)
-    board = newBoard
-    turnColor = nextColor
-    const { whiteCount, blackCount } = reversi.getDiskCount(board)
-    diskCount.whiteCount = whiteCount
-    diskCount.blackCount = blackCount
-    const setPBoard = reversi.setPutableCoord(nextColor, newBoard)
-    console.log({ board: board, x: x, y: y, t: nextColor })
-    if (turnColor === 9) {
-      const result = reversi.gameOver(board)
-      server.emit('gameOver', { result: result })
-      msg = geneWinnerMsg(result.winner)
-      isPlaying = false
-    } else {
-      msg = geneTurnPlayerMsg(nextColor)
-    }
+    const { newBoard, newNumberOfDisc } = rev.putDisc(x, y, gameState.turnColor, gameState.board)
+    const nextColor = rev.colorChange(gameState.turnColor, newBoard)
+    console.log({ board: newBoard, x: x, y: y, t: nextColor })
 
-    server.emit('gameInfo', {
-      board: setPBoard,
-      msg: msg,
-      diskCount: diskCount,
-      turnColor: entryPLKey[turnColor],
-    })
+    if (gameState.turnColor === rev.PLColor.NA) {
+      //const result = rev.gameOver(board)
+      //server.emit('gameOver', { result: result })
+      //msg = geneWinnerMsg(result.winner)
+      //isPlaying = false
+    } else {
+      gameState.board = newBoard
+      gameState.turnColor = nextColor
+      gameState.numberOfDisc = newNumberOfDisc
+      gameState.msg = geneTurnPlayerMsg(nextColor)
+
+      server.emit(
+        'gameInfo',
+        geneEmitData([rev.GSKey.board, rev.GSKey.turnColor, rev.GSKey.NumberOfDisc, rev.GSKey.msg])
+      )
+    }
   })
 
   socket.on('entry', () => {
-    const returnData = { type: 'entry', isSuccses: false }
+    let isSuccses = false
     console.log('entry:', socket.id)
     if (Object.keys(entryPL).length < 2 && !Object.values(entryPL).includes(socket.id)) {
-      console.log('log')
       //空の時ランダム
-      if (!Object.keys(entryPL).length) {
-        entryPL[entryPLKey[Math.floor(Math.random() * entryPLKey.length)]] = socket.id
-      } else if (!(entryPLKey[0] in entryPL)) {
-        entryPL[entryPLKey[0]] = socket.id
+      if (
+        Object.keys(entryPL).every((key) => {
+          entryPL[key] === ''
+        })
+      ) {
+        const randomKey: string =
+          Object.keys(entryPL)[Math.floor(Math.random() * Object.keys(entryPL).length)]
+        entryPL[randomKey] = socket.id
+      } else if (entryPL.black !== '') {
+        entryPL.white = socket.id
       } else {
-        entryPL[entryPLKey[1]] = socket.id
+        entryPL.black = socket.id
       }
-      returnData.isSuccses = true
+      isSuccses = true
     }
-    io.to(socket.id).emit('btnActionRep', returnData)
+    io.to(socket.id).emit('btnActionRep', isSuccses)
     console.log('EntryPL:', entryPL)
-
-    if (Object.keys(entryPL).length !== 2) {
-      msg = geneW8EntryMsg(Object.keys(entryPL).length)
-      console.log(msg)
-      io.emit('gameInfo', { msg: msg })
-    } else {
-      io.to(entryPL[entryPLKey[0]]).emit('startReversi', { color: entryPLKey[0] })
-      io.to(entryPL[entryPLKey[1]]).emit('startReversi', { color: entryPLKey[1] })
-      io.emit('startReversi', { color: null })
-      isPlaying = true
-      turnColor = 1
-      msg = geneTurnPlayerMsg(turnColor)
-      board = reversi.boardInit()
-      console.log('start', board)
-      io.emit('gameInfo', { board: reversi.setPutableCoord(turnColor, board), msg: msg })
+    if (!isSuccses) {
+      const nowEntryNum = Object.keys(entryPL).filter((key) => {
+        entryPL[key] === ''
+      }).length
+      if (nowEntryNum < 2) {
+        gameState.msg = geneW8EntryMsg(nowEntryNum)
+        io.emit('gameInfo', geneEmitData([rev.GSKey.msg]))
+      } else {
+        io.to(entryPL.White).emit('startReversi', { color: rev.colorConvEn[rev.PLColor.White] })
+        io.to(entryPL.Black).emit('startReversi', { color: rev.colorConvEn[rev.PLColor.Black] })
+        gameState.isPlaying = true
+        gameState.msg = geneTurnPlayerMsg(gameState.turnColor)
+        io.emit('gameInfo', geneEmitData([rev.GSKey.isPlaying, rev.GSKey.msg]))
+      }
     }
   })
 
   socket.on('cancel', () => {
-    const result = reversi.gameOver(board)
-    io.emit('gameOver', { result: result, isCancel: true })
-    msg = geneWinnerMsg(result.winner)
-    io.emit('gameInfo', { msg: msg, diskCount: diskCount })
-    isPlaying = false
-  })
-  socket.on('reset', () => {
-    board = reversi.board
-    turnColor = reversi.turnColor
-    entryPL = {}
-    diskCount = reversi.diskCount
-    msg = geneW8EntryMsg(Object.keys(entryPL).length)
-    server.emit('gameInfo', {
-      board: board,
-      msg: msg,
-      diskCount: diskCount,
-      turnColor: turnColor,
-    })
+    gameOverProcessing(true)
   })
 
+  socket.on('reset', () => {
+    resetGameState()
+    server.emit('gameInfo', gameState)
+  })
   socket.on('disconnect', () => {
     console.log('s', socket.id)
     if (Object.values(entryPL).includes(socket.id)) {
@@ -128,9 +138,13 @@ io.on('connection', function (socket: socketio.Socket) {
       if (delKey !== null) {
         delete entryPL[delKey]
         console.log(entryPL, delKey)
-        msg = geneW8EntryMsg(Object.keys(entryPL).length)
-        console.log(msg)
-        io.emit('gameInfo', { msg: msg })
+        if (gameState.isPlaying === false && gameState.isGameOver === false) {
+          gameState.msg = geneW8EntryMsg(Object.keys(entryPL).length)
+          console.log(gameState.msg)
+          io.emit('gameInfo', geneEmitData([rev.GSKey.msg]))
+        }
+      } else if (gameState.isPlaying === true && gameState.isGameOver === false) {
+        gameOverProcessing(true)
       }
     }
     console.log('disconnect', socket.id)
--- src/reversi.ts
+++ src/reversi.ts
@@ -1,41 +1,88 @@
 const SIZE = 8
 const WIDTH = SIZE
 const HEIGHT = SIZE
-const WHITE = 0,
-  BLACK = 1,
-  PUTABLE = 8,
-  EMPTY = 9
+export const Disc = {
+  White: 0,
+  Black: 1,
+  Putable: 8,
+  Empty: 9,
+} as const
 
-const conv = ['白', '黒']
-const diskCount = { whiteCount: 0, blackCount: 0 }
-const oppositeColor = (color: number) => (color === WHITE ? BLACK : color === BLACK ? WHITE : EMPTY)
+export const PLColor = {
+  White: 0,
+  Black: 1,
+  NA: 99,
+} as const
 
-const getDiskCount = (board: number[][]) => {
+export type Disc = typeof Disc[keyof typeof Disc]
+export type PLColor = typeof PLColor[keyof typeof PLColor]
+type Conv = { [key in PLColor]: string }
+export const colorConvJp: Conv = { 0: '白', 1: '黒', 99: '引き分け' }
+export const colorConvEn: Conv = { 0: 'White', 1: 'Black', 99: 'Drow' }
+
+export type NumberOfDisc = {
+  [key: string]: number
+  White: number
+  Black: number
+}
+
+export type EntryPL = {
+  [key: string]: string
+  White: string
+  Black: string
+}
+
+export const GSKey = {
+  board: 'board',
+  msg: 'msg',
+  turnColor: 'turnColor',
+  NumberOfDisc: 'NumberOfDisc',
+  isPlaying: 'isPlaying',
+  isGameOver: 'isGameOver',
+} as const
+
+export type GSKey = typeof GSKey[keyof typeof GSKey]
+
+export type GameStateType = Disc[][] | string | PLColor | NumberOfDisc | boolean
+
+export type GameState = {
+  [key: string]: Disc[][] | string | PLColor | NumberOfDisc | boolean
+  board: Disc[][]
+  msg: string
+  turnColor: PLColor
+  numberOfDisc: NumberOfDisc
+  isPlaying: boolean
+  isGameOver: boolean
+}
+
+const oppositeColor = (color: PLColor) => {
+  if (color === PLColor.White) return PLColor.Black
+  else if (color === PLColor.Black) return PLColor.White
+  return PLColor.NA
+}
+
+export const getNumberOfDisc = (board: Disc[][]) => {
   let x, y
-  let whiteCount = 0
-  let blackCount = 0
+  const result: NumberOfDisc = { White: 0, Black: 0 }
   for (y = 0; y < HEIGHT; y++) {
     for (x = 0; x < WIDTH; x++) {
-      if (board[y][x] == WHITE) {
-        whiteCount += 1
-      } else if (board[y][x] == BLACK) {
-        blackCount += 1
+      if (board[y][x] === Disc.White) {
+        result.white += 1
+      } else if (board[y][x] === Disc.Black) {
+        result.black += 1
       }
     }
   }
-  return { whiteCount: whiteCount, blackCount: blackCount }
+  return result
 }
 
-const isPutableDisk = (x: number, y: number, oneself: number, board: number[][]) => {
-  if (board[y][x] != EMPTY) {
+export const isPutableDisc = (x: number, y: number, oneself: PLColor, board: Disc[][]) => {
+  if (board[y][x] != Disc.Empty) {
     return false
   }
 
   const opponent = oppositeColor(oneself)
-  if (opponent === EMPTY) {
-    return false
-  }
-
+  if (opponent === PLColor.NA) return false
   for (let dx = -1; dx < 2; dx++) {
     for (let dy = -1; dy < 2; dy++) {
       if (dx === 0 && dy === 0) continue
@@ -52,7 +99,7 @@ const isPutableDisk = (x: number, y: number, oneself: number, board: number[][])
 
         if (tmpY < 0 || HEIGHT <= tmpY || tmpX < 0 || WIDTH <= tmpX) continue
 
-        if (board[tmpY][tmpX] === EMPTY) break
+        if (board[tmpY][tmpX] === Disc.Empty) break
 
         if (board[tmpY][tmpX] === oneself) return true
       }
@@ -61,39 +108,40 @@ const isPutableDisk = (x: number, y: number, oneself: number, board: number[][])
   return false
 }
 
-const setPutableCoord = (color: number, board: number[][]) => {
-  let x, y
-  const new_board = JSON.parse(JSON.stringify(board))
-  for (y = 0; y < HEIGHT; y++) {
-    for (x = 0; x < WIDTH; x++) {
-      if (isPutableDisk(x, y, color, board)) {
-        new_board[y][x] = PUTABLE
-      }
-    }
-  }
-  return new_board
+const setPutableArea = (turnColor: PLColor, board: Disc[][]): Disc[][] => {
+  return board.map((row, y) =>
+    row.map((_, x) => (isPutableDisc(x, y, turnColor, board) ? Disc.Putable : board[y][x]))
+  )
 }
 
-const boardInit = (): number[][] => {
-  const newBoard = Array.from(new Array(HEIGHT), () => new Array(WIDTH).fill(9))
-  newBoard[HEIGHT / 2][WIDTH / 2] = WHITE
-  newBoard[HEIGHT / 2 - 1][WIDTH / 2 - 1] = WHITE
-  newBoard[HEIGHT / 2 - 1][WIDTH / 2] = BLACK
-  newBoard[HEIGHT / 2][WIDTH / 2 - 1] = BLACK
+const fixPutableToEmpty = (board: Disc[][]): Disc[][] => {
+  return board.map((row, y) =>
+    row.map((num, x) => (num === Disc.Putable ? Disc.Empty : board[y][x]))
+  )
+}
+
+export const createBoard = (): Disc[][] => {
+  const board = Array.from(new Array(HEIGHT), () => new Array(WIDTH).fill(Disc.Empty))
+  board[HEIGHT / 2][WIDTH / 2] = Disc.White
+  board[HEIGHT / 2 - 1][WIDTH / 2 - 1] = Disc.White
+  board[HEIGHT / 2 - 1][WIDTH / 2] = Disc.Black
+  board[HEIGHT / 2][WIDTH / 2 - 1] = Disc.Black
+  const newBoard = setPutableArea(PLColor.Black, board)
   return newBoard
 }
 
-const board = boardInit()
-// eslint-disable-line
-const turnColor = BLACK
+export const putDisc = (
+  x: number,
+  y: number,
+  turnColor: PLColor,
+  board: Disc[][]
+): { newBoard: Disc[][]; newNumberOfDisc: NumberOfDisc } => {
+  if (board[y][x] != Disc.Putable)
+    return { newBoard: board, newNumberOfDisc: getNumberOfDisc(board) }
 
-const PutDisk = (x: number, y: number, oneself: number, board: number[][]) => {
-  const new_board = JSON.parse(JSON.stringify(board))
-
-  if (board[y][x] != EMPTY) return []
-
-  const opponent = oppositeColor(oneself)
-  if (opponent === EMPTY) return []
+  const opponent = oppositeColor(turnColor)
+  if (opponent === PLColor.NA) return { newBoard: board, newNumberOfDisc: getNumberOfDisc(board) }
+  const newBoard = fixPutableToEmpty(board)
 
   for (let dx = -1; dx < 2; dx++) {
     for (let dy = -1; dy < 2; dy++) {
@@ -111,59 +159,43 @@ const PutDisk = (x: number, y: number, oneself: number, board: number[][]) => {
 
         if (tmpY < 0 || HEIGHT <= tmpY || tmpX < 0 || WIDTH <= tmpX) continue
 
-        if (board[tmpY][tmpX] === EMPTY) break
+        if (board[tmpY][tmpX] === Disc.Empty) break
 
-        if (board[tmpY][tmpX] === oneself) {
-          new_board[y][x] = oneself
+        if (board[tmpY][tmpX] === turnColor) {
+          newBoard[y][x] = turnColor
           for (let n = 1; n < k; n++) {
-            new_board[y + dy * n][x + dx * n] = oneself
+            newBoard[y + dy * n][x + dx * n] = turnColor
           }
         }
       }
     }
   }
-  return new_board
+
+  return {
+    newBoard: setPutableArea(turnColor, newBoard),
+    newNumberOfDisc: getNumberOfDisc(newBoard),
+  }
 }
 
-const colorChange = (nowColor: number, board: number[][]) => {
+export const colorChange = (nowColor: PLColor, board: Disc[][]) => {
   const opponent = oppositeColor(nowColor)
   let x, y
   for (const color of [opponent, nowColor]) {
     for (y = 0; y < HEIGHT; y++) {
       for (x = 0; x < WIDTH; x++) {
-        if (isPutableDisk(x, y, color, board)) {
+        if (isPutableDisc(x, y, color, board)) {
           return color
         }
       }
     }
   }
-  return EMPTY
+  return PLColor.NA
 }
 
-const gameOver = (board: number[][]) => {
-  const { whiteCount, blackCount } = getDiskCount(board)
-  let winner
-
-  if (blackCount > whiteCount) {
-    winner = BLACK
-  } else if (whiteCount > blackCount) {
-    winner = WHITE
-  } else {
-    winner = EMPTY
-  }
-
-  return { winner: winner, whiteCount: whiteCount, blackCount: blackCount }
-}
-export = {
-  board: board,
-  turnColor: turnColor,
-  conv: conv,
-  diskCount: diskCount,
-  getDiskCount: getDiskCount,
-  isPutableDisk: isPutableDisk,
-  setPutableCoord: setPutableCoord,
-  boardInit: boardInit,
-  PutDisk: PutDisk,
-  colorChange: colorChange,
-  gameOver: gameOver,
+export const gameOver = (numberOfDisc: NumberOfDisc) => {
+  let winner: PLColor
+  if (numberOfDisc.black > numberOfDisc.white) winner = PLColor.Black
+  else if (numberOfDisc.white > numberOfDisc.black) winner = PLColor.White
+  else winner = PLColor.NA
+  return winner
 }
